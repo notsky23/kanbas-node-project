6713d2f1305defb8ebe5eac7cc520959c38395df
Notsky <notsky@gmail.com>
2024-04-05 15:51:45 -0400

    A5-Server

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import Hello from './Hello.js'
 import Lab5 from './Lab5.js'
 import cors from 'cors'
 import CourseRoutes from './Kanbas/Courses/routes.js'
+import ModuleRoutes from './Kanbas/Modules/routes.js'
+import AssignmentRoutes from './Kanbas/Assignments/routes.js'
 
 const app = express()
 app.use(cors())
@@ -11,5 +13,7 @@ app.use(express.json())
 Hello(app)
 Lab5(app)
 CourseRoutes(app)
+ModuleRoutes(app)
+AssignmentRoutes(app)
 
 app.listen(process.env.PORT || 4000);
---
+++ Kanbas/Assignments/routes.js
@@ -0,0 +1,45 @@
+import db from "../Database/index.js";
+
+function AssignmentRoutes(app) {
+    // Assignment - Create
+    app.post("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const newAssignment = {...req.body, course: cid, _id: new Date().getTime().toString()};
+        db.assignments.push(newAssignment);
+        res.send(newAssignment);
+    });
+
+    // Assignment - Read
+    app.get("/api/courses/:cid/assignments", (req, res) => {
+        const { cid } = req.params;
+        const assignments = db.assignments.filter((a) => a.course === cid);
+        res.send(assignments);
+    });
+
+    // Assignment - Update
+    app.put("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        const assignmentIndex = db.assignments.findIndex((a) => a._id === aid);
+        if(assignmentIndex !== -1) {
+            db.assignments[assignmentIndex] = {...db.assignments[assignmentIndex], ...req.body};
+            res.sendStatus(204);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+
+    // Assignment - Delete
+    app.delete("/api/assignments/:aid", (req, res) => {
+        const { aid } = req.params;
+        const initialLength = db.assignments.length;
+        db.assignments = db.assignments.filter((a) => a._id !== aid);
+        const newLength = db.assignments.length;
+        if(newLength < initialLength) {
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(404);
+        }
+    });
+}
+
+export default AssignmentRoutes;
