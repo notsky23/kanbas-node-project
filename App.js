5b711d81a1b7e27eb55216f2b164ad21a9abee9f
Notsky <notsky@gmail.com>
2024-04-21 19:38:28 -0400

    test

--- App.js
+++ App.js
@@ -45,6 +45,8 @@ const allowedDomains = [
   'https://a5--stalwart-gingersnap-b465f3.netlify.app',
   'https://a6--stalwart-gingersnap-b465f3.netlify.app',
   'https://main--stalwart-gingersnap-b465f3.netlify.app',
+  'https://harmonious-cannoli-f4be26.netlify.app',
+  'https://main--harmonious-cannoli-f4be26.netlify.app',
   process.env.FRONTEND_URL // Assuming this is set in your environment variables
 ];
 const corsOptions = {
